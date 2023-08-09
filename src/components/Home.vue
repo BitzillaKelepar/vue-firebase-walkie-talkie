bb821bf40b79685cdc926b5a65bdb72326fe7a66
Bitzilla <lucaraneri@hotmail.com>
2023-08-09 21:04:02 +0200

    add Login component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <h3>Home</h3>
+    <h3 class="text-3xl font-poppins">Home</h3>
+    <Login/>
   </div>
 </template>
+<script>
+import Login from "@/components/Login";
+export default {
+  components: {Login}
+}
+</script>
---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <aside>
+    <h3 class="text-2xl font-poppins">Sign-in Anonymously</h3>
+    <button @click="auth.signInAnonymously()" class="bg-sky-500 hover:bg-sky-700">Login</button>
+  </aside>
+</template>
+
+<script>
+import {auth} from "@/firebase";
+
+export default {
+  data() {
+    return {
+      auth,
+    }
+  },
+}
+</script>
