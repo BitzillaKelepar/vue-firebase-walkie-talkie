fe51fd3460f49ddb7a4c7f5595bbb8d504ec41cf
Bitzilla <lucaraneri@hotmail.com>
2023-08-11 20:13:11 +0200

    refactor: remove login router

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,8 @@
 <template>
   <div>
     <h3 class="text-3xl font-poppins">Home</h3>
-    <Login/>
   </div>
 </template>
 <script>
-import Login from "@/components/Login";
-export default {
-  components: {Login}
-}
+
 </script>
